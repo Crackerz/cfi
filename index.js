f2ed9d5bcf3a27f06dca95e41f86546fd8265665
William Blankenship <william.jblankenship@gmail.com>
2014-02-07 19:38:28 -0500

    Changed working directory back to original cwd. Tooled code with error messages.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var repo_url = "git@github.com:Marak/colors.js.git";
 var repo_path = process.env["HOME"]+"/.cfi";
 (function () {
-		console.log(repo_path);
 		//Logic for executing the file
 		var args = process.argv.splice(2);
 		switch(args[0]) {
@@ -34,6 +33,8 @@ function help() {
 
 //maintainRepo ensures our repo is being maintained and is updated
 function maintainRepo(callback) {
+	//TODO: Ensure git is installed
+
 	//File system maintenence
 	var fs = require('fs');
 	try {
@@ -50,30 +51,34 @@ function maintainRepo(callback) {
 	try {
 		process.chdir(repo_path);
 	} catch(error) {
+		console.log("Could not change directory to git repo!");
 		//TODO: handle error
 	}
-	//TODO: Ensure git is installed
+
 	var exec = require('child_process').exec;
 	var child = exec("git init", function(error, stdout,stderr) {
-		console.log("STDOUT: "+stdout);
-		console.log("STDERR: "+stderr);
-		if(error!=null) {
-			console.log("exec error: "+error);
-		}
+		execResult(error,stdout,stderr);
 		var child = exec("git remote add origin "+repo_url,function(error,stdout,stderr) {
-			console.log("STDOUT: "+stdout);
-			console.log("STDERR: "+stderr);
-			if(error!=null) {
-				console.log("exec error: "+error)
-			}
+			execResult(error,stdout,stderr);
 			var child = exec("git pull origin master",function(error,stdout,stderr) {
-					console.log("STDOUT: "+stdout);
-					console.log("STDERR: "+stderr);
-					if(error!=null) {
-						console.log("exec error: "+error)
-					}
+				if(!execResult(error,stdout,stderr)) {
+					console.log("exec error: "+error);
+				}
+				try {
+					process.chdir(cwd);
+				} catch(error) {
+					console.log("Could not change directory back to original cwd!");
+					//TODO: handle the unlikely event that we can't get back to where we started
+				}
 				callback();
 			})
 		});
 	});
+}
+
+function execResult(error, stdout,stderr) {
+	if(error!=null) {
+		return false;
+	}
+	return true;
 }
